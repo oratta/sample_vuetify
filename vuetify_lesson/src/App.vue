86a332873e55c85c506f98a8ac4b550d823e7838
oratta <oratta@gmail.com>
2019-12-10 18:59:19 +0900

    support menu

--- vuetify_lesson/src/App.vue
+++ vuetify_lesson/src/App.vue
@@ -5,8 +5,26 @@
       <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>Vuetify</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text>For Enterprise</v-btn>
-      <v-btn text>Support</v-btn>
+      <v-toolbar-items>
+        <v-btn text>For Enterprise</v-btn>
+        <v-menu offset-y>
+          <template v-slot:activator="{on}">
+            <v-btn v-on="on" text>Support</v-btn>
+          </template>
+          <v-list>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>Consulting and support</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>Discord community</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-toolbar-items>
     </v-app-bar>
     <v-footer color="primary" dark app>
       Vuetify
