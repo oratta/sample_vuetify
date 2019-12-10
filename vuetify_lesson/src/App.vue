7ed43c7c2613fb9785e57d5f37b78a444d61d009
oratta <oratta@gmail.com>
2019-12-10 19:13:24 +0900

    add icons

--- vuetify_lesson/src/App.vue
+++ vuetify_lesson/src/App.vue
@@ -9,13 +9,16 @@
         <v-btn text>For Enterprise</v-btn>
         <v-menu offset-y>
           <template v-slot:activator="{on}">
-            <v-btn v-on="on" text>Support</v-btn>
+            <v-btn v-on="on" text>Support<v-icon>mdi-menu-down</v-icon></v-btn>
           </template>
           <v-list>
             <v-subheader>Get help</v-subheader>
-            <v-list-item v-for="support in supports" :key="support">
+            <v-list-item v-for="support in supports" :key="support.name">
+              <v-list-item-icon>
+                <v-icon>{{ support.icon }}</v-icon>
+              </v-list-item-icon>
               <v-list-item-content>
-                <v-list-item-title>{{ support }}</v-list-item-title>
+                <v-list-item-title>{{ support.name }}</v-list-item-title>
               </v-list-item-content>
             </v-list-item>
           </v-list>
@@ -35,11 +38,11 @@ export default {
     return{
       drawer: null,
       supports: [
-              'Consulting and support',
-              'Discord community',
-              'Report a bug',
-              'Github issue board',
-              'Stack overview',
+              {name: 'Consulting and support', icon: 'mdi-vuetify'},
+              {name: 'Discord community', icon: 'mdi-discord'},
+              {name: 'Report a bug', icon: 'mdi-bug'},
+              {name: 'Github issue board', icon: 'mdi-github-face'},
+              {name: 'Stack overview', icon: 'mdi-stack-overflow'},
       ]
     }
   }
