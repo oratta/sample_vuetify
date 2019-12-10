c6c36ad0b356c19d795fb5a406251ae2c9c3aa71
oratta <oratta@gmail.com>
2019-12-10 18:52:24 +0900

    add button

--- vuetify_lesson/src/App.vue
+++ vuetify_lesson/src/App.vue
@@ -4,6 +4,9 @@
     <v-app-bar color="primary" dark app clipped-left>
       <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>Vuetify</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text>For Enterprise</v-btn>
+      <v-btn text>Support</v-btn>
     </v-app-bar>
     <v-footer color="primary" dark app>
       Vuetify
