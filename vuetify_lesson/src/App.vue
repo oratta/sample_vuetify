2b97fab0c8d28a7088a38de08360373251f3629f
oratta <oratta@gmail.com>
2019-12-10 18:39:59 +0900

    footer

--- vuetify_lesson/src/App.vue
+++ vuetify_lesson/src/App.vue
@@ -3,6 +3,9 @@
     <v-app-bar color="primary" dark app>
       <v-toolbar-title>Vuetify</v-toolbar-title>
     </v-app-bar>
+    <v-footer color="primary" dark app>
+      Vuetify
+    </v-footer>
   </v-app>
 </template>
 
