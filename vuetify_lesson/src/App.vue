7568b259e4dab783507d02d6750cdb2c2bc73137
oratta <oratta@gmail.com>
2019-12-12 14:12:37 +0900

    add content

--- vuetify_lesson/src/App.vue
+++ vuetify_lesson/src/App.vue
@@ -73,6 +73,9 @@
         </v-menu>
       </v-toolbar-items>
     </v-app-bar>
+    <v-content>
+      <router-view />
+    </v-content>
     <v-footer color="primary" dark app>
       Vuetify
     </v-footer>
