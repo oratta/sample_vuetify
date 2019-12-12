f4717afa008e6921354b3cf61f2b0ead9d488678
oratta <oratta@gmail.com>
2019-12-12 14:37:57 +0900

    add graph

--- vuetify_lesson/src/views/Home.vue
+++ vuetify_lesson/src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <SaleGraph />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SaleGraph from '../components/SaleGraph.vue'
 
 export default {
-  name: 'home',
   components: {
-    HelloWorld
+    SaleGraph
   }
 }
 </script>
